5a6e9e8e839996d644854d014a81335c39f5cd30
Alek Michelson <am7590@rit.edu>
2022-06-16 21:51:09 -0400

    Logo endpoint

---
+++ iex_service.py
@@ -0,0 +1,50 @@
+import requests
+
+
+class IEXStock:
+
+    def __init__(self, token, symbol):
+        self.BASE_URL = 'https://cloud.iexapis.com/stable'
+
+        self.token = token
+        self.symbol = symbol
+
+    def get_logo(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/logo?token={self.token}"
+        r = requests.get(url)
+        return r.json()
+
+    def get_company_info(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/company?token={self.token}"
+        r = requests.get(url)
+        response = r.json()
+        # st.write(response)
+        return response
+
+    def get_stats(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/advanced-stats?token={self.token}"
+        r = requests.get(url)
+        return r.json()
+
+    def get_company_news(self, last=20):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/news/last/{last}?token={self.token}"
+        r = requests.get(url)
+        return r.json()
+
+    def get_dividends(self, range='5y'):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/dividends/{range}?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_institutional_ownership(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/institutional-ownership?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
+
+    def get_insider_transactions(self):
+        url = f"{self.BASE_URL}/stock/{self.symbol}/insider-transactions?token={self.token}"
+        r = requests.get(url)
+
+        return r.json()
--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Union
+"""
+Steps to run in a venv:
+
+python -m venv .venv
+source .venv/bin/activate
+pip install uvicorn
+pip install fastapi
+pip install redis
+pip install requests
+uvicorn main:app --reload
+"""
+
 from fastapi import FastAPI
+import config
+import redis
+import json
+from iex_service import IEXStock
 
 app = FastAPI()
-
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/logo/{ticker}")
+def read_logo(ticker: str):
+    stock = IEXStock(config.IEX_KEY, ticker)
+    logo_key = f"{ticker}_logo"
+    logo = redis_client.get(logo_key)
+
+    if logo is None:
+        print("Retrieving data from API...")
+        logo = stock.get_logo()
+        redis_client.set(logo_key, json.dumps(logo))
+    else:
+        print("Retrieving data from cache...")
+        logo = json.loads(logo)
+
+    return json.dumps(logo)
